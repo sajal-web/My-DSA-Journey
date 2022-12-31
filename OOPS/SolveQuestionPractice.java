d943460af306dd837b2ad09fce60d5732c15aef1
sajal-web <mahatosajal65@gmail.com>
2022-12-31 11:28:38 +0530

    solve question 1

---
+++ OOPS/SolveQuestionPractice.java
@@ -0,0 +1,13 @@
+public class SolveQuestionPractice{
+    public static void main(String args[]){
+        Student sajal = new Student();
+        sajal.name = "Sajal";
+        System.out.println(sajal.name);
+    }
+}
+// Question 1
+// FInd out the correct statement to assign name to object
+class Student{
+    int roll;
+    String name;
+}
