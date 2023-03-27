e180cba7a1c55cb20c83b1b8eb22c127c1f73a71
sajal-web <mahatosajal65@gmail.com>
2023-03-27 20:02:12 +0530

    print subarray

---
+++ OOPS/Practice/arrays/subarray.java
@@ -0,0 +1,16 @@
+public class subarray{
+    public static void main(String args[]){
+        int numbers[] = { 1,2,3,4,5,6,7,8,9};
+        printSubarray(numbers);
+    }
+    static void printSubarray(int numbers[]){
+        for(int i = 0;i<numbers.length;i++){
+            int curr = numbers[i];
+            for(int j = i+1;j<numbers.length;j++){
+                System.out.print("("+curr +" , "+ numbers[j]+")");
+            }
+            System.out.println();
+        }
+    }
+}
+// total number of pair is A.P --> n(n-1)/2; arithmatic progress
