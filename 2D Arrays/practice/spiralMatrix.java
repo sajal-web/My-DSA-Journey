603aafc79cb2079031f1ed462b00ab32676ba901
sajal-web <mahatosajal65@gmail.com>
2023-04-20 22:47:08 +0530

    initial commit

---
+++ 2D Arrays/practice/spiralMatrix.java
@@ -0,0 +1,19 @@
+public class spiralMatrix{
+    public static void main(String args[]){
+        int matrix[][] = {
+
+            {1,2,3,4,5},
+            {6,7,8,9,10},
+            {11,12,13,14,15}
+        };
+        sprialMatrixPrintFun(matrix);
+    }
+  static void sprialMatrixPrintFun(int matrix[][]){
+        // lets define the necessary variables
+        int startRow = 0, startColumn = 0, endRow=matrix.length-1,
+            endColumn = matrix[0].length-1;
+
+       
+        
+    }
+}
